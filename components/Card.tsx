9f5a0cc69696c4903fa2a99c3f5096fda94bcb9b
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2022-03-29 20:18:46 +0530

    added card component

---
+++ components/Card.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='w-[95%] h-[100%] flex-none bg-blue-300 snap-center rounded-[20px]'>
+
+    </div>
+  )
+}
+
+export default Card
