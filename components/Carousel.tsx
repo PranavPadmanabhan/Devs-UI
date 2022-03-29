47172a305aa8cef97d804eed92ffde40cf709c6e
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2022-03-29 20:20:18 +0530

    added card component

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Card from './Card'
 
 function Carousel() {
-  return (
-    <div className="flex w-[90vw] h-[65vh] bg-red-300 items-center justify-start box-border overflow-x-scroll snap-x snap-mandatory px-[2%] scroll-px-4 scrollbar-hide">
-        <Card />
-      <Card />
-      <Card />
+    return (
+        <div className="flex w-[90vw] h-[65vh] bg-red-300 items-center justify-start box-border overflow-x-scroll snap-x snap-mandatory px-[2%] scroll-px-4 scrollbar-hide">
+            <Card />
+            <Card />
+            <Card />
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Carousel
